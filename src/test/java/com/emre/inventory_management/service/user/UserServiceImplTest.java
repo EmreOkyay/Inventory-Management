27f652d56f16571858ecc14724b4b45db3138576
EmreOkyay <okyayatilla@hotmail.com>
2025-11-30 01:29:11 +0300

    mockito extension

--- src/test/java/com/emre/inventory_management/service/user/UserServiceImplTest.java
+++ src/test/java/com/emre/inventory_management/service/user/UserServiceImplTest.java
@@ -8,7 +8,9 @@ import com.emre.inventory_management.repository.UserRepository;
 import com.emre.inventory_management.event.UserEventProducer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +18,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
     @Mock
